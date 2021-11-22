d2d58434b0195451981580782d4e350cc7c32de3
Georges Chaudy <chaudyg@gmail.com>
2021-11-22 16:45:28 +0100

    First pass implementation

---
+++ go.mod
@@ -0,0 +1,49 @@
+module github.com/grafana/certmagic-gcs
+
+go 1.17
+
+require (
+	cloud.google.com/go/storage v1.18.2
+	github.com/caddyserver/certmagic v0.15.2
+	github.com/fsouza/fake-gcs-server v1.30.2
+	github.com/stretchr/testify v1.7.0
+	google.golang.org/api v0.60.0
+)
+
+require (
+	cloud.google.com/go v0.97.0 // indirect
+	github.com/census-instrumentation/opencensus-proto v0.2.1 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cncf/udpa/go v0.0.0-20201120205902-5459f2c99403 // indirect
+	github.com/cncf/xds/go v0.0.0-20210312221358-fbca930ec8ed // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/envoyproxy/go-control-plane v0.9.9-0.20210512163311-63b5d3c536b0 // indirect
+	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/libdns/libdns v0.2.1 // indirect
+	github.com/mholt/acmez v1.0.1 // indirect
+	github.com/miekg/dns v1.1.43 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
+	golang.org/x/sys v0.0.0-20211025201205-69cdffdb9359 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20211021150943-2b146023228c // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ storage.go
@@ -0,0 +1,196 @@
+package certmagicgcs
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/caddyserver/certmagic"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+var (
+	// LockExpiration is the duration before wich a Lock is considered expired
+	LockExpiration = 1 * time.Minute
+	// LockPollInterval is the interval between each check of the lock state.
+	LockPollInterval = 1 * time.Second
+)
+
+// Storage implements certmagic#Storage
+// https://pkg.go.dev/github.com/caddyserver/certmagic#Storage
+type Storage struct {
+	bucket *storage.BucketHandle
+}
+
+func NewStorage(ctx context.Context, bucketName string, opts ...option.ClientOption) (*Storage, error) {
+	client, err := storage.NewClient(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("could not initialize storage client: %v", err)
+	}
+	bucket := client.Bucket(bucketName)
+	return &Storage{bucket: bucket}, nil
+}
+
+// Store puts value at key.
+func (s *Storage) Store(key string, value []byte) error {
+	ctx := context.Background()
+	obj := s.bucket.Object(key)
+
+	w := obj.NewWriter(ctx)
+	if _, err := w.Write(value); err != nil {
+		return fmt.Errorf("writing object %s: %v", key, err)
+	}
+	return w.Close()
+}
+
+// Load retrieves the value at key.
+func (s *Storage) Load(key string) ([]byte, error) {
+	rc, err := s.bucket.Object(key).NewReader(context.Background())
+	if err != nil {
+		return nil, fmt.Errorf("loading object %s: %v", key, err)
+	}
+	defer rc.Close()
+	return ioutil.ReadAll(rc)
+}
+
+// Delete deletes key. An error should be
+// returned only if the key still exists
+// when the method returns.
+func (s *Storage) Delete(key string) error {
+	if err := s.bucket.Object(key).Delete(context.Background()); err != nil {
+		return fmt.Errorf("deleting object %s: %v", key, err)
+	}
+	return nil
+}
+
+// Exists returns true if the key exists
+// and there was no error checking.
+func (s *Storage) Exists(key string) bool {
+	_, err := s.bucket.Object(key).Attrs(context.Background())
+	return err != storage.ErrObjectNotExist
+}
+
+// List returns all keys that match prefix.
+// If recursive is true, non-terminal keys
+// will be enumerated (i.e. "directories"
+// should be walked); otherwise, only keys
+// prefixed exactly by prefix will be listed.
+func (s *Storage) List(prefix string, recursive bool) ([]string, error) {
+	query := &storage.Query{Prefix: prefix}
+	if !recursive {
+		query.Delimiter = "/"
+	}
+	var names []string
+	it := s.bucket.Objects(context.Background(), query)
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("listing objects: %v", err)
+		}
+		if attrs.Name != "" {
+			names = append(names, attrs.Name)
+		}
+	}
+	return names, nil
+}
+
+// Stat returns information about key.
+func (s *Storage) Stat(key string) (certmagic.KeyInfo, error) {
+	var keyInfo certmagic.KeyInfo
+	attr, err := s.bucket.Object(key).Attrs(context.Background())
+	if err != nil {
+		return keyInfo, fmt.Errorf("loading attributes for %s: %v", key, err)
+	}
+	keyInfo.Key = key
+	keyInfo.Modified = attr.Updated
+	keyInfo.Size = attr.Size
+	keyInfo.IsTerminal = true
+	return keyInfo, nil
+}
+
+// Lock acquires the lock for key, blocking until the lock
+// can be obtained or an error is returned. Note that, even
+// after acquiring a lock, an idempotent operation may have
+// already been performed by another process that acquired
+// the lock before - so always check to make sure idempotent
+// operations still need to be performed after acquiring the
+// lock.
+//
+// The actual implementation of obtaining of a lock must be
+// an atomic operation so that multiple Lock calls at the
+// same time always results in only one caller receiving the
+// lock at any given time.
+//
+// To prevent deadlocks, all implementations (where this concern
+// is relevant) should put a reasonable expiration on the lock in
+// case Unlock is unable to be called due to some sort of network
+// failure or system crash. Additionally, implementations should
+// honor context cancellation as much as possible (in case the
+// caller wishes to give up and free resources before the lock
+// can be obtained).
+func (s *Storage) Lock(ctx context.Context, key string) error {
+	lockKey := s.objLockName(key)
+	obj := s.bucket.Object(lockKey)
+	for {
+		attrs, err := obj.Attrs(ctx)
+		// create the lock if it doesn't exists
+		if err == storage.ErrObjectNotExist {
+			w := obj.NewWriter(ctx)
+			if _, err := w.Write([]byte{}); err != nil {
+				return fmt.Errorf("creating %s: %v", lockKey, err)
+			}
+			if err := w.Close(); err != nil {
+				return fmt.Errorf("closing %s: %v", lockKey, err)
+			}
+			continue
+		} else if err != nil {
+			return fmt.Errorf("loading attributes %s: %v", lockKey, err)
+		}
+		// Acquire the lock
+		if !attrs.TemporaryHold {
+			if _, err := obj.Update(ctx, storage.ObjectAttrsToUpdate{TemporaryHold: true}); err != nil {
+				return fmt.Errorf("setting temporary hold on object %s: %v", lockKey, err)
+			}
+			return nil
+		}
+		// Unlock if the lock expired
+		if attrs.Updated.Add(LockExpiration).Before(time.Now().UTC()) {
+			if err := s.Unlock(key); err != nil {
+				return fmt.Errorf("unlocking expired lock %s: %v", lockKey, err)
+			}
+			continue
+		}
+		// Wait and try again
+		time.Sleep(LockPollInterval)
+	}
+}
+
+// Unlock releases the lock for key. This method must ONLY be
+// called after a successful call to Lock, and only after the
+// critical section is finished, even if it errored or timed
+// out. Unlock cleans up any resources allocated during Lock.
+func (s *Storage) Unlock(key string) error {
+	lockKey := s.objLockName(key)
+	obj := s.bucket.Object(lockKey)
+	_, err := obj.Update(context.Background(), storage.ObjectAttrsToUpdate{TemporaryHold: false})
+	if err == storage.ErrObjectNotExist {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("could not remove temporary hold for %s: %v", lockKey, err)
+	}
+	if err := s.Delete(s.objLockName(key)); err != nil {
+		return fmt.Errorf("delting lock %s: %v", lockKey, err)
+	}
+	return nil
+}
+
+func (s *Storage) objLockName(key string) string {
+	return key + ".lock"
+}
